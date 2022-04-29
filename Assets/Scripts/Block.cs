0e8ec41621b60d79cec13789e79f40f23c1afa40
Nick <kolan.eremeev1001@gmail.com>
2022-04-30 02:04:43 +0300

    block type added

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private BlockType _blockType;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public BlockType BlockType => _blockType;
 }
