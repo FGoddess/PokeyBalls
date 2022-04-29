34a111909b86235f5f5fc4d7f83b4a1ea6c8f4a3
Nick <kolan.eremeev1001@gmail.com>
2022-04-30 02:03:23 +0300

    ball'n'block interaction / bug fixes

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float _jumpForce = 1f;
+    [SerializeField] private Pole _pole;
+    [SerializeField] private float _jumpForce = 5f;
+
+    private float _jumpForceMultiplier = 1.5f;
+
     private Rigidbody _rigidbody;
 
-    private bool _isFixed;
-    private bool _canThrow = true;
+    public bool IsFixed => _rigidbody.isKinematic;
 
     private void Awake()
     {
@@ -18,27 +21,55 @@ public class Ball : MonoBehaviour
     {
         if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit))
         {
-            if (hit.collider.TryGetComponent(out Block block) && !_canThrow)
+            if (hit.collider.TryGetComponent(out Block block))
             {
-                _rigidbody.isKinematic = true;
-                _rigidbody.velocity = Vector3.zero;
-                _isFixed = true;
+                _pole.Activate();
+                _pole.transform.position = new Vector3(_pole.transform.position.x, transform.position.y, _pole.transform.position.z);
+
+                switch (block.BlockType)
+                {
+                    case BlockType.Fixable:
+                        {
+                            _rigidbody.isKinematic = true;
+                            break;
+                        }
+
+                    case BlockType.Unfixable:
+                        {
+                            _pole.Hide(0.1f);
+                            break;
+                        }
+
+                    case BlockType.Damagable:
+                        {
+                            _pole.Hide(0.1f);
+                            break;
+                        }
+
+                    case BlockType.Destructable:
+                        {
+                            break;
+                        }
+
+                    case BlockType.Finish:
+                        {
+                            _rigidbody.isKinematic = true;
+                            Debug.Log("EndLevel");
+                            break;
+                        }
+                };
             }
         }
     }
 
-    public void TryThrow()
+    public void TryThrow(float value)
     {
-        if (_isFixed)
-        {
-            _isFixed = false;
-            _canThrow = true;
-            return;
-        }
+        if (!IsFixed) return;
 
-        _rigidbody.isKinematic = false;
         _rigidbody.velocity = Vector3.zero;
-        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        _canThrow = false;
+        _rigidbody.isKinematic = false;
+
+        _rigidbody.AddForce(Vector3.up * (_jumpForce + value * _jumpForceMultiplier), ForceMode.Impulse);
+        _rigidbody.AddTorque(Vector3.right * (_jumpForce + value * _jumpForceMultiplier), ForceMode.Impulse);
     }
 }
