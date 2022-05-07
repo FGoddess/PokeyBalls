8674b55837a4e7c6663641862a9b4b5b4d9b3d0e
Nick <kolan.eremeev1001@gmail.com>
2022-05-07 13:37:55 +0300

    finish reward fixing

--- Assets/Scripts/Tower/Finish.cs
+++ Assets/Scripts/Tower/Finish.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
     [SerializeField] private int _goldForHit;
 
     public int GoldForHit => _goldForHit;
+
+    private void Awake()
+    {
+        GetComponentInChildren<Text>().text = _goldForHit.ToString();
+    }
 }
